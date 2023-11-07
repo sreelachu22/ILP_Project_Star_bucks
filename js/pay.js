39a169d76f2241f6b2e52392fd06c82c9b91658b
Sreelakshmi P M <sreelakshmi.pm@experionglobal.local>
2023-11-07 09:25:37 +0530

    feat:-Login page almost done

--- js/pay.js
+++ js/pay.js
@@ -1,40 +1,14 @@
-
-// function payLogin() {
-//     const activePlayer = 0;
-//     const congratsMessage = "Login";
-
-//     const popup = document.createElement('div');
-//     popup.className = 'popup';
-//     popup.textContent = congratsMessage;
-
-//     popup.style.position = 'fixed';
-//     popup.style.left = '30%';
-//     popup.style.right = '30%';
-//     popup.style.top = '15%';
-//     popup.style.bottom = '15%';
-//     popup.style.width = "40%";
-//     popup.style.height = "70%";
-//     popup.style.borderRadius = "30px"
-//     popup.style.backgroundColor = 'white';
-//     popup.style.padding = '20px';
-//     popup.style.border = '2px solid black';
-//     popup.style.textAlign = 'center';
-
-//     document.body.appendChild(popup);
-// }
-
-// Open the login modal
 function payLogin() {
     document.getElementById('loginModal').style.display = 'block';
+    document.getElementById('payPage').style.display = 'block';
   }
   
-  // Close the login modal
+
   function closeLoginModal() {
     document.getElementById('loginModal').style.display = 'none';
     window.location.href = 'home.html';
   }
   
-  // Close the modal if the user clicks outside the modal
   window.onclick = function(event) {
     var modal = document.getElementById('loginModal');
     if (event.target == modal) {
@@ -42,3 +16,18 @@ function payLogin() {
     }
   }
   
+
+  function togglePasswordVisibility() {
+    var passwordField = document.getElementById("passwordField");
+    var toggleIcon = document.getElementById("togglePassword");
+  
+    if (passwordField.type === "password") {
+      passwordField.type = "text";
+      toggleIcon.innerHTML = "&#128064;";
+    } else {
+      passwordField.type = "password";
+      toggleIcon.innerHTML = "&#128065;";
+    }
+  }
+
+
