3d6709f4602b39e8b6bb1422a7b44ae146c088e3
Sreelakshmi P M <sreelakshmi.pm@experionglobal.local>
2023-11-08 14:32:10 +0530

    feat:- added get help page for pay login

--- js/pay.js
+++ js/pay.js
@@ -1,3 +1,5 @@
+//!Sreelakshmi
+//popup window display
 function payLogin() {
     document.getElementById('loginModal').style.display = 'block';
     document.getElementById('payPage').style.display = 'block';
@@ -6,6 +8,7 @@ function payLogin() {
 
   function closeLoginModal() {
     document.getElementById('loginModal').style.display = 'none';
+    document.getElementById('payPage').style.display = 'none';
     window.location.href = 'home.html';
   }
   
@@ -16,10 +19,10 @@ function payLogin() {
     }
   }
   
-
+  //Password visibility function. We can see the password if we click the eye icon
   function togglePasswordVisibility() {
-    var passwordField = document.getElementById("passwordField");
-    var toggleIcon = document.getElementById("togglePassword");
+    var passwordField = document.getElementById("username");
+    var toggleIcon = document.getElementById("password");
   
     if (passwordField.type === "password") {
       passwordField.type = "text";
@@ -30,4 +33,70 @@ function payLogin() {
     }
   }
 
+
+// Get the input fields and the Login button
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const loginButton = document.getElementById('loginButton');
+
+// Add event listeners to the input fields
+usernameInput.addEventListener('input', toggleButtonState);
+passwordInput.addEventListener('input', toggleButtonState);
+
+// Function to toggle the Login button state
+function toggleButtonState() {
+  const usernameValue = usernameInput.value.trim();
+  const passwordValue = passwordInput.value.trim();
+
+  if (usernameValue !== '' && passwordValue.length >= 8) {
+    loginButton.removeAttribute('disabled');
+  } else {
+    loginButton.setAttribute('disabled', 'true');
+  }
+}
+
+
+
+
+//Get help session javascript starts here
+function getHelp() {
+  document.getElementById('helpModal').style.display = 'block';
+  document.getElementById('gettingHelp').style.display = 'block';
+}
+
+const usernameHelpInput = document.getElementById('username-help');
+const dobInput = document.getElementById('dob');
+const resetButton = document.getElementById('resetButton');
+
+// Add event listeners to the input field
+usernameHelpInput.addEventListener('input', toggleResetButtonState);
+dobInput.addEventListener('input', toggleResetButtonState);
+
+// Function to toggle the reset button state
+function toggleResetButtonState() {
+  const usernameValue = usernameHelpInput.value.trim();
+  const dobValue = dobInput.value;
+
+  if (usernameValue !== '' && dobValue !== '') {
+    resetButton.removeAttribute('disabled');
+  } else {
+    resetButton.setAttribute('disabled', 'true');
+  }
+}
+
+// To ensure focus on the date input field
+const dateInput = document.getElementById('dobInput');
+
+dateInput.addEventListener('click', function() {
+  simulateClick();
+});
+
+function simulateClick() {
+  const event = new MouseEvent('click', {
+    view: window,
+    bubbles: true,
+    cancelable: true
+  });
+  dateInput.dispatchEvent(event);
+}
 
