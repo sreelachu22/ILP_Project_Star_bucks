9a448cb727a9bcd058c534da88a81f9c7ba413d3
Nathaniel Yeldo <nathaniel.yeldo@experionglobal.local>
2023-11-21 18:54:27 +0530

    fix:- dynamically fetched job board data

--- js/script.js
+++ js/script.js
@@ -1,52 +1,84 @@
-function openApplicationModal(jobTitle) {
-  // Set the job title in the modal
-  document.getElementById("jobTitle").innerText = jobTitle;
-  // Show the modal
-  $("#applicationModal").modal("show");
-}
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbyPMR-lSqrbGtOTCiymHyX14Uro2gVID3EzGPdwhF6_1aIIdK28HKR9cdDw09AGRF4/exec";
 
-function submitApplication() {
-  // Client-side form validation using HTML5 Constraint Validation API
+const form = document.forms["applicationForm"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
   if (document.getElementById("applicationForm").checkValidity()) {
-    // Gather form data
-    const fullName = document.getElementById("fullName").value;
-    const email = document.getElementById("email").value;
-    const resume = document.getElementById("resume").files[0];
-
-    // Validate and process the data (you may need to send it to a server)
-
-    // Reset the form
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+      .then((response) => {
+        //   alert("Thank you! your form is submitted successfully.");
+      })
+      .then(() => {
+        window.location.reload();
+      })
+      .catch((error) => console.error("Error!", error.message));
     document.getElementById("applicationForm").reset();
-
-    // Close the modal
     $("#applicationModal").modal("hide");
-
-    // Export data to Excel (you may need to implement server-side logic for this)
-    exportToExcel(fullName, email, resume);
   } else {
-    // If the form is not valid, show the validation feedback
     document.getElementById("applicationForm").classList.add("was-validated");
+  }
+});
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   fetch(scriptURL, { method: "POST", body: new FormData(form) })
+//     .then((response) =>
+//       alert("Thank you! your form is submitted successfully.")
+//     )
+//     .then(() => {
+//       window.location.reload();
+//     })
+//     .catch((error) => console.error("Error!", error.message));
+// });
+
+async function fetchData() {
+  try {
+    const response = await fetch(
+      "https://api-generator.retool.com/Z4W4JZ/data"
+    );
+    if (!response.ok) {
+      throw new Error(`API request failed with status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+    const course = data;
+    let randomId = [];
+    const coursesContainer = document.getElementById("coursesContainer");
+
+    const row = document.createElement("div");
+    row.className = "row justify-content-center";
+
+    for (let i = 0; i < 10; i++) {
+      randomId[i] = Math.floor(Math.random() * course.length);
+      const courseElement = document.createElement("div");
+      courseElement.className = "courseElement col-md-5 mb-4 mx-3"; // Adjust the width and margin as needed
+
+      const jobTitle = document.createElement("h2");
+      jobTitle.textContent = `${course[randomId[i]].Jobs}`;
+      const jobDesc = document.createElement("p");
+      jobDesc.textContent = `${course[randomId[i]].Description}`;
+      const salary = document.createElement("p");
+      salary.textContent = `₹ ${course[randomId[i]].Salary}`;
+      const applyNow = document.createElement("button");
+      if (applyNow) {
+        applyNow.className = "applyNow section-btn btn btn-primary btn-block";
+        applyNow.setAttribute("data-toggle", "modal");
+        applyNow.setAttribute("data-target", "#applicationModal");
+      }
+      applyNow.textContent = "Apply Now";
+      courseElement.appendChild(jobTitle);
+      courseElement.appendChild(jobDesc);
+      courseElement.appendChild(salary);
+      courseElement.appendChild(applyNow);
+      row.appendChild(courseElement);
+    }
+
+    coursesContainer.appendChild(row);
+  } catch (error) {
+    console.error("error:", error);
   }
 }
 
-function exportToExcel(fullName, email, resume) {
-  // Create a workbook and worksheet
-  const wb = XLSX.utils.book_new();
-  const ws = XLSX.utils.json_to_sheet([
-    {
-      "Full Name": fullName,
-      Email: email,
-      Resume: resume ? resume.name : "Not uploaded",
-    },
-  ]);
-
-  // Add the worksheet to the workbook
-  XLSX.utils.book_append_sheet(wb, ws, "Application Data");
-
-  // Create a blob with the workbook's content
-  const blob = XLSX.write(wb, { bookType: "xlsx", type: "blob" });
-
-  // Save the blob as an Excel file
-  saveAs(blob, "application_data.xlsx");
-}
-
+fetchData();
