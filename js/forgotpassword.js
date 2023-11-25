144d51324c7feb50262cf5868647df757f6c8cfd
Sreelakshmi P M <sree2000lachu@gmail.com>
2023-11-25 15:30:55 +0530

    feat:-Added firebase authentication in signup,sign in, sending confirmation mail, sending reset password mail, sending otp to mobile number

--- js/forgotpassword.js
+++ js/forgotpassword.js
@@ -1,15 +1,53 @@
-function forgotPassword() {
-    let resetEmail = document.getElementById("forgot-email");
-    let validEmailExpr =
-      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if (resetEmail.value === "") {
-      alert("Email cannot be empty");
-    } else if (!resetEmail.value.match(validEmailExpr)) {
-      alert("Not a valid email address");
-    } else {
-      alert(
-        `An email with password recovery link has been sent to ${resetEmail.value}`
-      );
-    }
-  }
+
+const firebaseConfig = {
+  apiKey: "AIzaSyChHTWfu8OOy0CH1bZA-G9j2FEXOIiHR5c",
+  authDomain: "starbucks-authentication.firebaseapp.com",
+  projectId: "starbucks-authentication",
+  storageBucket: "starbucks-authentication.appspot.com",
+  messagingSenderId: "373089171674",
+  appId: "1:373089171674:web:74b8143b69937bcab3cde3",
+  measurementId: "G-G5J0Q2F35X",
+};
+firebase.initializeApp(firebaseConfig);
+
+const forgotPasswordEmail = document.getElementById("forgot-email");
+let resetPasswordButton = document.getElementById("resetpassword");
+
+
+let forgotPassword = () => {
+  firebase
+    .auth()
+    .sendPasswordResetEmail(forgotPasswordEmail.value)
+  .then(() => {
+    // Password reset email sent!
+    // ..
+    alert(`Password reset link send to ${forgotPasswordEmail.value}`)
+    window.location.href = '/home.html';
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+}
+
+resetPasswordButton.addEventListener("click", function() {
+  forgotPassword();
+});
+
+
+// function forgotPassword() {
+//     let resetEmail = document.getElementById("forgot-email");
+//     let validEmailExpr =
+//       /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+//     if (resetEmail.value === "") {
+//       alert("Email cannot be empty");
+//     } else if (!resetEmail.value.match(validEmailExpr)) {
+//       alert("Not a valid email address");
+//     } else {
+//       alert(
+//         `An email with password recovery link has been sent to ${resetEmail.value}`
+//       );
+//     }
+//   }
 
--- js/pay.js
+++ js/pay.js
@@ -1,70 +1,50 @@
-//!Payment page Validation
-//Using fakeapi manually created an api which contains 12 data
-// (contains card number, card type, CV code and card owner).
-// Using the api checked whether the entered payment are already exist.
-//If exist then a payment successful popup will displayed.
-//else an alert message displayed
-function confirmPayment(){
- fetch('https://mocki.io/v1/8519910e-5506-426f-a86d-7f592fd77d21')//manually created api
-    .then(response => response.json())
-    .then(data => {
-        const cards = [];
-        data.forEach(element => {
-            cards.push(element);
-            console.log(cards)
-        });
-        //Data entered through payment form
-          let enteredCardNumber = document.getElementById('cardNumber').value;
-          let enteredCardType = document.getElementById('cardType').value;
-          let enteredCVCode = document.getElementById('cvCode').value;
-          let enteredCardOwner = document.getElementById('cardOwner').value;
-          let paidAmount = document.getElementById('amount').value;
+// //!Payment page Validation
+// document.getElementById('confirmPayment').addEventListener('click', function () {
+//   confirmPayment();
+// });
 
-          localStorage.setItem('enteredCardNumber', enteredCardNumber);
-          localStorage.setItem('enteredCardType', enteredCardType);
-          localStorage.setItem('enteredCVCode', enteredCVCode);
-          localStorage.setItem('enteredCardOwner', enteredCardOwner);
-          localStorage.setItem('paidAmount', paidAmount);
-          //validation check
-          for (let i = 0; i < cards.length; i++) {
-              if (enteredCardNumber == cards[i].cardNumber &&
-                  enteredCardType == cards[i].cardType &&
-                  enteredCVCode == cards[i].cvCode &&
-                  enteredCardOwner == cards[i].cardOwner) {
-                    console.log("success")
-                  Successful("Payment successful");
-                  return;
-              }
-          }
-          alert('Enter valid details');
-    })  
-    .catch(error => console.error('Error:', error));
-  }
+// function confirmPayment() {
+//   fetch('https://mocki.io/v1/8519910e-5506-426f-a86d-7f592fd77d21') //manually created api
+//      .then(response => response.json())
+//      .then(data => {
+//        const cards = [];
+//        data.forEach(element => {
+//          cards.push(element);
+//          console.log(cards)
+//        });
+ 
+//        // Data entered through payment form
+//        let enteredCardNumber = document.getElementById('cardNumber').value;
+//        let enteredCardType = document.getElementById('cardType').value;
+//        let enteredCVCode = document.getElementById('cvCode').value;
+//        let enteredCardOwner = document.getElementById('cardOwner').value;
+//        let paidAmount = document.getElementById('amount').value;
+ 
+//        // Validation check
+//        for (let i = 0; i < cards.length; i++) {
+//          if (enteredCardNumber == cards[i].cardNumber &&
+//            enteredCardType == cards[i].cardType &&
+//            enteredCVCode == cards[i].cvCode &&
+//            enteredCardOwner == cards[i].cardOwner &&
+//            paidAmount != '') {
+//            let successMessage = "Card Number: " + enteredCardNumber.replace(/.(?=.{4})/g, "x") + ",\n";
+//            successMessage += "Card Owner: " + enteredCardOwner + ",\n";
+//            successMessage += "Amount Paid: " + paidAmount;
+//            document.getElementById(
+//             "qrcode"
+//           ).src = `https://quickchart.io/qr?text=${successMessage}`;
+ 
+//            $('#successful').modal('show');
+//            return;
+//          }
+//        }
 
-//!Payment successful
-//Display a popup containing green tick and success message
-//If closed, redirect to home.html
-function Successful(msg) {
-  let successful = document.createElement('div');
-  successful.setAttribute("id", 'successful');
-
-  let greenTick = document.createElement('img');
-  greenTick.src = '/img/green_tic.png';
-  greenTick.classList.add("greenTick")
-
-  let successMessage = document.createElement('p');
-  successMessage.setAttribute("id", 'successMessage');
-  successMessage.classList.add("successMessage");
-  successMessage.innerText = msg;
-
-  successful.appendChild(greenTick);
-  successful.appendChild(successMessage);
-
-  document.body.appendChild(successful);
-  successful.style.display = 'block';
-
-  setTimeout(function () {
-    successful.style.display = 'none';
-    window.location.href = 'home.html';
-  }, 3000);
-}
+//        alert('Enter valid details');
+//      })
+//      .catch(error => console.error('Error:', error));
+//  }
+ 
+//  function closePaymentSuccessful(){
+//   $('#successful').modal('hide');
+//   window.location.href = "home.html";
+//  }
